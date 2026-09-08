845f3e46b0673c6af6646ddcb33a28520bd60ab5
Alfredo <alfredo.nathaniel@ui.ac.id>
2026-09-08 20:03:10 +0700

    feat: complete tutorial 2

---
+++ main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Alfredo",
+        "npm": "2506656412",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Be great or just be good"
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Alfredo",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
--- portofolio/settings.py
+++ portofolio/settings.py
@@ -41,6 +41,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
